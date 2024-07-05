b2cb8d89b522cff6f5fd8775300c5e96d45f1263
Squiblydoo <77356206+Squiblydoo@users.noreply.github.com>
2024-07-05 11:02:14 -0400

    2.0 Add VT capabilities

--- README.md
+++ README.md
@@ -1,47 +1,99 @@
 # CertReport
 
-This script is intended to reduce the load of effort required to report authenticode certificates.
-That is, if the file is on MalwareBazzar, a user can use the script to generate information to submit to the Certificate Issuer. For example:
+This tool is intended to reduce the load of effort required to report authenticode certificates. It is intended to take the smallest amount of effort from the reporter, but provide the certificate authority with most the information they need to make a decision. When possible, it is recommended to augment the report with your own findings to help the certificate provider know what suspicious indicators you found.
+
+As of version 2, we have added support to use VirusTotal API. In order to allow for VirusTotal API, we have added additional functions. 
+
+The default behavior of cert report is to query MalwareBazaar, which does not require an API key.
+
+**Note: In version 2, it is required to provide the `--hash` switch**
+ Here is an example:
 Calling the script and passing in a SHA256 like this:<br>
-`.\certReport 89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9`
+`certReport --hash 89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9`
 
 Will print the following information to the console:
 
 ```
+---------------------------------
 Greetings,
- We identified a malware signed with an SSL.com EV Code Signing Intermediate CA RSA R3 certificate. 
+ We identified a malware signed with a SSL.com EV Code Signing Intermediate CA RSA R3 certificate. 
 The malware sample is available on MalwareBazaar here: https://bazaar.abuse.ch/sample/89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9
 Here are the signature details:
 Name: A.P.Hernandez Consulting s.r.o.
 Issuer: SSL.com EV Code Signing Intermediate CA RSA R3
 Serial Number: 2941d5f8758501f9dbc4ba158058c3b5
 SHA256 Thumbprint: a982917ba6de9588f0f7ed554223d292524e832c1621acae9ad11c0573df54a5
-
+Valid From: 2024-01-25T16:51:40Z
+Valid Until: 2025-01-24T16:51:40Z
 The malware was tagged as exe, Pikabot and signed.
 MalwareBazaar submitted the file to multiple public sandboxes, the links to the sandbox results are below:
-Sandbox  / Malware Family        /  Verdict      / Analysis URL
-Intezer          None    unknown         https://analyze.intezer.com/analyses/c4915ef4-198f-4aba-81ed-81b29cd4dce6?utm_source=MalwareBazaar 
-Triage   pikabot         10 / 10         https://tria.ge/reports/240222-pqlqkshb2w/ 
-VMRay    Pikabot         malicious       https://www.vmray.com/analyses/_mb/89dc50024836/report/overview.html 
+Sandbox	 / Malware Family	 /  Verdict	 / Analysis URL
+Intezer 	 None 	 unknown 	 https://analyze.intezer.com/analyses/c4915ef4-198f-4aba-81ed-81b29cd4dce6?utm_source=MalwareBazaar 
+Triage 	 pikabot 	 10 / 10	 https://tria.ge/reports/240222-pqlqkshb2w/ 
+VMRay 	 Pikabot 	 malicious 	 https://www.vmray.com/analyses/_mb/89dc50024836/report/overview.html 
 
 Please let us know if you have any questions.
 ------------------------
-Send the above message to the certificate provider. Here is where to send some:
+Send the above message to the certificate provider. 
 This report should be sent to SSL.com: https://ssl.com/revoke
 ```
 
-This information is to be provided to the Certificate Issuer using the appropriate abuse report channels (such as email or website).
-This workflow provides the Certificate Issuer information about the certificate and other details to help their decision support leading to the revocation of certificate
+This information is to be provided to the Certificate Issuer using the appropriate abuse report channels (such as email or website). The appropriate channel is provided at the end of the report (see above).
 
-# Report in Detail
-The script uses MalwareBazaar's API to pull information from MalwareBazaar. Currently, the script prioritizes pulling back information from sandboxes that have public reports. The goal is to provide as much information to support decisions as well as allowing the Certificate Issuer to have additional avenues for investigation.<br>
-When reporting certificates, Certificate Issuers need to know the Serial Number and Thumbprint associated with the malicious certificate. This report makes that easily accessible.<br>
-The report also raises the "Tags" from MalwareBazaar. These tags are associated with any human or machine placed tags that provide searchable indicators. In the example above, the Pikabot sample surfaces "exe", "Pikabot", and "signed". I recommend cleaning up the report manually and including additional details if possible, but it is not required. The benefit in doing so is that it provides the Certificate Issuer more decision support and increases the likelihood that they they will revoke the certificate.<br>
+## Using VirusTotal
+In version 2, it became possible to query VirusTotal. To use VirusTotal first set up your API key using the following command.
+```
+certReport --setup <API_KEY_GOES_HERE>
+```
+This will create a .env file in your current working directory, this will allow certReport to retrieve the API key from this directory. (We don't have a better way to do this on all OS at the moment. Submit a PR if you have a good alternative.)
+
+Once the API key is configured the following command will generate a report:
+```
+certReport --hash 89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9 --service virustotal
+```
+
+Alternatively, the switches can be simplified:
+
+```
+certReport -# 89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9 -s VT
+```
+Both commands will return the following report: 
+```
+---------------------------------
+Greetings,
+ We identified a malware signed with a  SSL.com EV Code Signing Intermediate CA RSA R3 certificate. 
+The malware sample is available on VirusTotal here: https://www.virustotal.com/gui/file/89dc50024836f9ad406504a3b7445d284e97ec5dafdd8f2741f496cac84ccda9/detection
+
+Here are the signature details:
+Name: A.P.Hernandez Consulting s.r.o.
+Issuer:  SSL.com EV Code Signing Intermediate CA RSA R3
+Serial Number: 56 B6 29 CD 34 BC 78 F6
+Thumbprint: 743AF0529BD032A0F44A83CDD4BAA97B7C2EC49A
+Valid From: 2017-05-31 18:14:37
+Valid Until: 2042-05-30 18:14:37
+
+The malware was tagged as a peexe, long-sleeps, spreader, detect-debug-environment, service-scan, overlay, revoked-cert, signed and checks-user-input.
+The malware was detected by 50 out of 74 antivirus engines.
+The malware was classified as trojan by 30 detection engines.
+The file was flagged as pikabot by 23 detection engines, zusy by 6 detection engines and gdfvt by 2 detection engines
+
+This file was found during our investigation and had the following suspicious indicators:
+ - The file triggered the following high IDS rules: ET CNC Feodo Tracker Reported CnC Server group 1, ET CNC Feodo Tracker Reported CnC Server group 1, ET CNC Feodo Tracker Reported CnC Server group 2, ET CNC Feodo Tracker Reported CnC Server group 4, ET CNC Feodo Tracker Reported CnC Server group 5, ET CNC Feodo Tracker Reported CnC Server group 6, ET CNC Feodo Tracker Reported CnC Server group 8, ET CNC Feodo Tracker Reported CnC Server group 10, ET CNC Feodo Tracker Reported CnC Server group 11, ET CNC Feodo Tracker Reported CnC Server group 13, ET CNC Feodo Tracker Reported CnC Server group 14, ET CNC Feodo Tracker Reported CnC Server group 15 and ET CNC Feodo Tracker Reported CnC Server group 16
+
+Please let us know if you have any questions.
+------------------------
+Send the above message to the certificate provider. 
+This report should be sent to SSL.com: https://ssl.com/revoke
+```
+
+As stated previously, it is recommended to add additional bulletpoints near the end of the report. Additional bulletpoints should include findings from your own investigation. These details can help provide decision support for the certificate provider.
 
 ## Contributing
 Please feel free changes to the script for additional certificate provider email addresses or methods of reporting. Half of the battle in reporting is finding where certificates should be submitted.
 
 # Why Report?
-Starting in 2018, the majority of certificates were no longer stolen, but they are issued to imposters. I call these "Imposter Certs" or "Sus Certs". Squiblydoo's research and findings can be read here: https://squiblydoo.blog/2023/05/12/certified-bad/, a scholarly article on this behavior can be found here: http://users.umiacs.umd.edu/~tdumitra/papers/WEIS-2018.pdf
+Starting in 2018, the majority of certificates were no longer stolen, but they are issued to impostors (this case is argued in a scholarly article here: http://users.umiacs.umd.edu/~tdumitra/papers/WEIS-2018.pdf). I call these "Impostor Certs". 
+In 2023, I published my research into 50 certificates used by one actor. My findings confirmed that certificates are used to sign multiple malware families: https://squiblydoo.blog/2023/05/12/certified-bad/.
+In 2024, I published an article on Impostor certs, after having revoked 100 certificates used to sign the same malware, that article can be read here: https://squiblydoo.blog/2024/05/13/impostor-certs/.
 
 The TLDR is that multiple actors use the same certificate and reporting a certificate raises the cost of signing for all threat actors and it can impact multiple malware campaigns.
--- src/certReport/main.py
+++ src/certReport/main.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
 import requests
 import json
 import argparse
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+version = "2.0"
 
 def create_tag_string(tags):
     if len(tags) == 0:
@@ -16,74 +19,169 @@ def create_tag_string(tags):
         tag_string += " and " + tags[-1]
         return tag_string
 
-def main():
-    parser = argparse.ArgumentParser(description = "Pull data pertaining to filehash from MalwareBazzar by specifying hash associated with the malware.")
-    parser.add_argument("hash", help="Specify hash of file to query.")
-    args = parser.parse_args()
-    #Make request for Data
-    query = {"query": "post-data", "query": "get_info", "hash": args.hash}
+def query_malwarebazaar(filehash):
+    query = {"query": "post-data", "query": "get_info", "hash": filehash}
     data_request = requests.post("https://mb-api.abuse.ch/api/v1/", data=query)
-
     data_request.raise_for_status()
     json_string = data_request.text
     json_python_value = json.loads(json_string)
-    if json_python_value["data"][0]["code_sign"]:
-        subject_cn = json_python_value["data"][0]["code_sign"][0]["subject_cn"]
-        issuer_cn = json_python_value["data"][0]["code_sign"][0]["issuer_cn"]
-        serial_number = json_python_value["data"][0]["code_sign"][0]["serial_number"]
-        thumbprint = json_python_value["data"][0]["code_sign"][0]["thumbprint"]
+    return json_python_value
 
-        tags = json_python_value["data"][0]["tags"]
-        tag_string = create_tag_string(tags)
-        vendor_intel_dict = json_python_value["data"][0]["vendor_intel"]
+def query_virustotal(filehash):
+    try:
+        api_key = os.environ['VT_API_KEY']
+    except KeyError:
+        print("Please set your VirusTotal API key by running the script with the argument '--setup <API-KEY>.")
+        exit()
+    headers = {"accept": "application/json", "x-apikey": api_key}
+    item_id = {"id": filehash}
+    data_request = requests.get("https://www.virustotal.com/api/v3/files/" + filehash, headers=headers)
+    data_request.raise_for_status()
+    json_python_value = data_request.json()
+    return json_python_value
+
+def print_reporting_instructions(issuer_cn):
+    print("")
+    print("Please let us know if you have any questions.")
+    print("------------------------")
+    print('''Send the above message to the certificate provider. ''')
+    if "SSL" in issuer_cn:
+        print("This report should be sent to SSL.com: https://ssl.com/revoke")
+    elif "Certum" in issuer_cn:
+        print("This report should be sent to Certum PL: https://problemreport.certum.pl/")
+    elif "Digicert" in issuer_cn:
+        print("This report should be sent to Digicert: Revoke@digicert.com")
+    elif "GlobalSign" in issuer_cn:
+        print("This report should be sent to GlobalSign: report-abuse@globalsign.com")
+    elif "Sectigo" in issuer_cn:
+        print("This report should be sent to Sectigo: signedmalwarealert@sectigo.com")
+    elif "Entrust" in issuer_cn:
+        print("This report should be sent to Entrust: ecs.support@entrust.com")
+    else:
+        print("Assuming this is a valid certificate. Search the provider's website for the reporting email.")
+
+def process_virustotal_data(json_python_value, filehash):
+    signers = json_python_value["data"]["attributes"]["signature_info"]["signers"]
+    signer_list = signers.split(";")
+    subject_cn = signer_list[0]
+    issuer_cn = signer_list[1]
+    x509_details = json_python_value["data"]["attributes"]["signature_info"]["x509"][0]
+    serial_number = x509_details["serial number"]
+    thumbprint = x509_details["thumbprint"]
+    valid_from = x509_details["valid from"]
+    valid_to = x509_details["valid to"]
+
+    stats = json_python_value["data"]["attributes"]["last_analysis_stats"]
+    tags = json_python_value["data"]["attributes"]["tags"]
+    tag_string = create_tag_string(tags)
     
-        print("\n---------------------------------\nGreetings,\n "
-            "We identified a malware signed with an " + issuer_cn + " certificate. \n" 
-            "The malware sample is available on MalwareBazaar here: https://bazaar.abuse.ch/sample/" + args.hash + "\n"\
-            "Here are the signature details:\n"\
-                "Name: " + subject_cn + "\n"
-                "Issuer: " + issuer_cn + "\n"
-                "Serial Number: " + serial_number + "\n"
-                "SHA256 Thumbprint: " + thumbprint + "\n"
-                "\n"
-                "The malware was tagged as " + tag_string + "."
-                "\n"
-                "MalwareBazaar submitted the file to multiple public sandboxes, the links to the sandbox results are below:\n"
-                "Sandbox\t / Malware Family\t /  Verdict\t / Analysis URL"
-                )
-                
-        for key, value in vendor_intel_dict.items():
-            if key == 'ANY.RUN':
-                print(f"{key} \t {value[0]['malware_family']}\t {value[0]['verdict']}\t {value[0]['analysis_url']}")
-            elif key == 'Triage':
-                print(f"{key} \t {value['malware_family']} \t {value['score']} / 10\t {value['link']} ")
-            elif key == 'Intezer':
-                print(f"{key} \t {value['family_name']} \t {value['verdict']} \t {value['analysis_url']} ")
-            elif key == 'VMRay':
-                print(f"{key} \t {value['malware_family']} \t {value['verdict']} \t {value['report_link']} ")
-        
-        print("")
-        print("Please let us know if you have any questions.")
-        print("------------------------")
-        print('''Send the above message to the certificate provider. ''')
-        if "SSL" in issuer_cn:
-            print("This report should be sent to SSL.com: https://ssl.com/revoke")
-        elif "Certum" in issuer_cn:
-            print("This report should be sent to Certum PL: ccp@certum.pl")
-        elif "Digicert" in issuer_cn:
-            print("This report should be sent to Digicert: Revoke@digicert.com")
-        elif "GlobalSign" in issuer_cn:
-            print("This report should be sent to GlobalSign: report-abuse@globalsign.com")
-        elif "Sectigo" in issuer_cn:
-            print("This report should be sent to Sectigo: signedmalwarealert@sectigo.com")
-        elif "Entrust" in issuer_cn:
-            print("This report should be sent to Entrust: ecs.support@entrust.com")
-        else:
-            print("Assuming this is a valid certificate. Search the provider's website for the reporting email.")
+    print("\n---------------------------------\nGreetings,\n "
+        "We identified a malware signed with a " + issuer_cn + " certificate. \n"
+        "The malware sample is available on VirusTotal here: https://www.virustotal.com/gui/file/" + filehash + "/detection\n\n"\
+        "Here are the signature details:\n"\
+            "Name: " + subject_cn + "\n"
+            "Issuer: " + issuer_cn + "\n"
+            "Serial Number: " + serial_number + "\n"
+            "Thumbprint: " + thumbprint + "\n"
+            "Valid From: " + valid_from + "\n"
+            "Valid Until: " + valid_to +
+            "\n\n"
+            "The malware was tagged as a " + tag_string + "."
+            "\n"
+            "The malware was detected by " + str(stats["malicious"]) + " out of " + str(stats["harmless"] + stats["failure"] + stats["malicious"] + stats["suspicious"] + stats["undetected"]) + " antivirus engines."
+            )
+    if json_python_value["data"]["attributes"]["popular_threat_classification"]:
+        threat_type = json_python_value["data"]["attributes"]["popular_threat_classification"]["popular_threat_category"][0]
+        print("The malware was classified as " + threat_type["value"] + " by " + str(threat_type["count"]) + " detection engines.")
+        threat_name = json_python_value["data"]["attributes"]["popular_threat_classification"]["popular_threat_name"]
+        threat_name_list = []
+        for threat in threat_name:
+            threat_name_list.append(threat["value"] + " by " + str(threat["count"]) + " detection engines")
+        threat_name_string = create_tag_string(threat_name_list)
+        print("The file was flagged as " + threat_name_string)
 
+    print("\nThis file was found during our investigation and had the following suspicious indicators:")
+    # Additional evidence of malicious behavior can be found by HIGH IDS rules. Will consider other data later.
+    high_ids_rules = []
+    if json_python_value["data"]["attributes"]["crowdsourced_ids_results"]:
+        for rule in json_python_value["data"]["attributes"]["crowdsourced_ids_results"]:
+            if rule["alert_severity"] == "high":
+                high_ids_rules.append(rule["rule_msg"])
+    high_ids_rules_list = create_tag_string(high_ids_rules)
+    if  len(high_ids_rules) > 0:
+        print(" - The file triggered the following high IDS rules: " + high_ids_rules_list)
 
-        
+    print_reporting_instructions(issuer_cn)
+
+def process_malwarebazaar_data(json_python_value, filehash):
+    subject_cn = json_python_value["data"][0]["code_sign"][0]["subject_cn"]
+    issuer_cn = json_python_value["data"][0]["code_sign"][0]["issuer_cn"]
+    serial_number = json_python_value["data"][0]["code_sign"][0]["serial_number"]
+    thumbprint = json_python_value["data"][0]["code_sign"][0]["thumbprint"]
+    valid_from = json_python_value["data"][0]["code_sign"][0]["valid_from"]
+    valid_until = json_python_value["data"][0]["code_sign"][0]["valid_to"]
+
+    tags = json_python_value["data"][0]["tags"]
+    tag_string = create_tag_string(tags)
+    vendor_intel_dict = json_python_value["data"][0]["vendor_intel"]
+
+    print("\n---------------------------------\nGreetings,\n "
+        "We identified a malware signed with a " + issuer_cn + " certificate. \n" 
+        "The malware sample is available on MalwareBazaar here: https://bazaar.abuse.ch/sample/" + filehash + "\n"\
+        "Here are the signature details:\n"\
+            "Name: " + subject_cn + "\n"
+            "Issuer: " + issuer_cn + "\n"
+            "Serial Number: " + serial_number + "\n"
+            "SHA256 Thumbprint: " + thumbprint + "\n"
+            "Valid From: " + valid_from + "\n"
+            "Valid Until: " + valid_until +
+            "\n"
+            "The malware was tagged as " + tag_string + "."
+            "\n"
+            "MalwareBazaar submitted the file to multiple public sandboxes, the links to the sandbox results are below:\n"
+            "Sandbox\t / Malware Family\t /  Verdict\t / Analysis URL"
+            )
+            
+    for key, value in vendor_intel_dict.items():
+        if key == 'ANY.RUN':
+            print(f"{key} \t {value[0]['malware_family']}\t {value[0]['verdict']}\t {value[0]['analysis_url']}")
+        elif key == 'Triage':
+            print(f"{key} \t {value['malware_family']} \t {value['score']} / 10\t {value['link']} ")
+        elif key == 'Intezer':
+            print(f"{key} \t {value['family_name']} \t {value['verdict']} \t {value['analysis_url']} ")
+        elif key == 'VMRay':
+            print(f"{key} \t {value['malware_family']} \t {value['verdict']} \t {value['report_link']} ")
+
+    print_reporting_instructions(issuer_cn)
+
+def create_or_update_env_file(api_key):
+    with open('.env', 'w') as env_file:
+        env_file.write(f'VT_API_KEY={api_key}\n')
+    print("API key has been saved to .env file.")
+
+def main():
+    parser = argparse.ArgumentParser(description = "Pull data pertaining to filehash by specifying hash associated with the malware and choosing a provider (defaults to MalwareBazaar).")
+    parser.add_argument("-#","--hash", help="Specify hash of file to query.")
+    parser.add_argument("-s", "--service", default="malwarebazaar", choices=["malwarebazaar", "VT", "virustotal"],
+                        help="Select the service to query (default: malwarebazaar).")
+    parser.add_argument('--setup', metavar='API_KEY', type=str, help='Setup your API key by passing the API key as an argument.')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + version)
+
+    args = parser.parse_args()
+
+    if args.setup:
+        create_or_update_env_file(args.setup)
+        exit()
     
+    if not args.hash:
+        parser.error("the following arguments are required: --hash")
+
+    if args.service == "virustotal" or args.service == "VT":
+        json_python_value = query_virustotal(args.hash)
+        process_virustotal_data(json_python_value, args.hash)
+    else:  # Default to MalwareBazaar
+        json_python_value = query_malwarebazaar(args.hash)
+        process_malwarebazaar_data(json_python_value, args.hash)
             
 if __name__=="__main__":
     main()
